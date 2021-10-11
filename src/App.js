c3c7be2053e39aedf0514e3d22d09c6e94f8c08e
pierluigi <pierluigibaiano@gmail.com>
2021-10-11 15:12:49 +0200

    create netlify function

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import "./App.css";
 import WeatherFetch from "./Components/WeatherFetch";
+import Api from "./.netlify/functions/api";
 
+let serverURL;
+
+fetch(".netlify/functions/api")
+  .then((response) => response.json())
+  .then((json) => {
+    serverURL = json.api;
+  });
 
 function App() {
-
   return (
     <div className="App">
       <h1 className="appLabel">React Weather App</h1>
       <WeatherFetch />
-      
-
     </div>
   );
 }
