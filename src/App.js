22ab91f3f095b40c9acb440d060a00a2e0b57892
pierluigi <pierluigibaiano@gmail.com>
2021-10-12 09:19:17 +0200

    delete redundant code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
 import "./App.css";
 import WeatherFetch from "./Components/WeatherFetch";
-import Api from "./.netlify/functions/api";
 
-let serverURL;
-
-fetch(".netlify/functions/api")
-  .then((response) => response.json())
-  .then((json) => {
-    serverURL = json.api;
-  });
 
 function App() {
   return (
