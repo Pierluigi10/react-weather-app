d807c1c7e015e905497859abfd8d8a17bfafd48e
pierluigi <pierluigibaiano@gmail.com>
2021-10-13 11:38:00 +0200

    add momentjs (sunset, sunrise - add country

--- src/Components/WeatherFetch.js
+++ src/Components/WeatherFetch.js
@@ -1,8 +1,9 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState } from "react";
+import moment from "moment";
 
 const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
-
+// const API_KEY = "b7e88f5530d434448d216c34fb206639";
 
 function WeatherFetch() {
   // const city = "berlin"
@@ -14,10 +15,13 @@ function WeatherFetch() {
   const [humidity, setHumidity] = useState("");
   const [id, setId] = useState("");
   const [iconID, setIconID] = useState([]);
+  const [country, setCountry] = useState("");
+  const [sunrise, setSunrise] = useState("");
+  const [sunset, setSunset] = useState("");
 
   const loadPageData = async () => {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
-    // const url = `https://api.openweathermap.org/data/2.5/weather?q=ber&appid=${API_KEY}`;
+    // const url = `api.openweathermap.org/data/2.5/forecast?id={city ID}&appid={API key}`;
     const response = await fetch(url);
     if (response.ok) {
       const data = await response.json();
@@ -32,6 +36,11 @@ function WeatherFetch() {
       setDisplayCity(city.charAt(0).toUpperCase() + city.slice(1));
       // console.log(data.id);
       setId(data.id);
+      setCountry(
+        data.sys.country.charAt(0) + data.sys.country.charAt(1).toLowerCase()
+      );
+      setSunrise(data.sys.sunrise);
+      setSunset(data.sys.sunset);
       console.log(data);
     }
   };
@@ -62,7 +71,9 @@ function WeatherFetch() {
             placeholder="Choose your city"
           />
         </label>
-        <button className="button" type="submit" >submit</button>
+        <button className="button" type="submit">
+          submit
+        </button>
       </form>
 
       {id && (
@@ -75,7 +86,7 @@ function WeatherFetch() {
                 href={`https://en.wikipedia.org/wiki/${displayCity}`}
                 rel="noreferrer"
               >
-                {displayCity}
+                {displayCity}, {country}
               </a>
             </span>
             : <span className="numbers">{mainTemp}</span>℃
@@ -93,6 +104,14 @@ function WeatherFetch() {
               src={`http://openweathermap.org/img/wn/${iconID}@2x.png`}
               alt="icon"
             />
+          </div>
+          <div className="moreInfo">
+          <p className="sunrise">Sunrise:{" "}  
+            {moment.unix(sunrise).format("hh:mm:ss a")}
+          </p>
+          <p className="sunset">Sunset:{" "}
+            {moment.unix(sunset).format("hh:mm:ss a")}
+          </p>
           </div>
         </div>
       )}
